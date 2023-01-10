946f95b95b49cf8d2fe9c66cd61dd6b2a634c77f
Matthew <52621525+H11Maitree@users.noreply.github.com>
2023-01-10 22:40:57 +0800

    Initial Commit

---
+++ ThaiQR_POS/ThaiQR_POSApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThaiQR_POSApp.swift
+//  ThaiQR_POS
+//
+//  Created by Maitree Hirunteeyakul on 1/10/23.
+//
+
+import SwiftUI
+
+@main
+struct ThaiQR_POSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
